d20d0f544b8fde50262e00d816aa70ac9d4023e1
okathira <43328895+okathira@users.noreply.github.com>
2024-09-10 06:59:05 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# yarn の node version が違う
+
+## link 検索
+
+search-links.ps1
---
+++ index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env cmd
+
+console.log(process.version);
+console.log(__dirname);
+console.log(process.execPath);
+// console.log(process.env);
